7c7b73bbdc7190a4683c2ed9615ada84e02a3dfe
Finn Skinner <finnski100@gmail.com>
2025-10-21 16:10:25 +0100

    Created the nessassery files for the project

---
+++ Stenography.py
@@ -0,0 +1,12 @@
+def main():
+    #produce a key here look into it
+    return 0
+
+
+def encrypt_data(audio_data, key):
+    encrypted_audio_data = audio_data
+    return encrypted_audio_data
+
+def store_data(video_data, audio_data):
+    #put the data in one another and return a video output
+    return 0
