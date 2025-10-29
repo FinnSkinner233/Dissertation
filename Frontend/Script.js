1fc57029f9d726803e5434b656dc624c3343e0fd
Finn Skinner <finnski100@gmail.com>
2025-10-29 21:49:58 +0000

    added previews for the video and the audio

--- Frontend/Script.js
+++ Frontend/Script.js
@@ -2,8 +2,23 @@ document.getElementById("videoInput").addEventListener("change", function(event)
     var preview = document.getElementById("videoPreview")
 
     if(event.target.files[0]){
-        var URL = URL.createObjectURL(file);
-        preview.src = URL;
+        var fileURL = URL.createObjectURL(event.target.files[0]);
+        preview.src = fileURL;
+        preview.style.display = "block"
+    }
+    else{
+        preview.src = ""
+        preview.style.display = "none"
+    }
+    
+});
+
+document.getElementById("audioInput").addEventListener("change", function(event){
+    var preview = document.getElementById("audioPreview")
+
+    if(event.target.files[0]){
+        var fileURL = URL.createObjectURL(event.target.files[0]);
+        preview.src = fileURL;
         preview.style.display = "block"
     }
     else{
