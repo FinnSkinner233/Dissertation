7e72b034da08c43940eec9481e90762ac65d7c80
Finn Skinner <finnski100@gmail.com>
2025-11-04 14:49:21 +0000

    Saved the video frame data to an array

--- Stenography.py
+++ Stenography.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from cryptography.fernet import Fernet
+import cv2
+import os
 
 app = Flask(__name__)
 
@@ -16,9 +18,19 @@ def encode():
     video_file = request.files.get('video')
     audio_file = request.files.get('audio')
 
-    #makes sure both are stores or errors out
+
+    #makes sure both are stored or errors out
     if not video_file or not audio_file:
         return "Both video and audio files are required", 400
+
+    #save and capture the video
+    video_path = "temp_video.avi"
+    video_file.save(video_path)
+    video_capture = cv2.VideoCapture(video_path)
+
+    #make sure the video is captured
+    if not video_capture.isOpened():
+        return "Video could not be opened"
     
     #get the data stream of the audio file
     audio_data = audio_file.read()
@@ -28,10 +40,23 @@ def encode():
     fernet = Fernet(key)
     encrypted_audio = fernet.encrypt(audio_data)
 
-
     #convert the audio data into binary
     binary_audio = ''.join(format(byte,'08b') for byte in encrypted_audio)
-    print (binary_audio)
+
+
+    #create and array to store the frames of the video
+    video_frames = []
+    #read the frames of the video
+    #ret is a boolean determined if there is a frame to read
+    #frame is the data of the frame
+    while True:
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+        video_frames.append(frame)
+
+    video_capture.release()
+    os.remove(video_path)
 
     #confirms the audio and video files have been stored
     return "Files have been uploaded sucsesfully"
